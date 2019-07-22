d66364811509526042d3dc628824e259d0239062
mrstephenneal <stephen.neal14@gmail.com>
2019-07-22 13:15:28 -0400

    MAKE validation_domain env
    ADD use of replace_domain to inject validation domain to nginx.conf.

--- 1.17-alpine-v1/scripts/entrypoint.sh
+++ 1.17-alpine-v1/scripts/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Replace @VALIDATION_DOMAIN placeholder with env variable value
+replace_domain --domain ${validation_domain} \
+    --conf-file /etc/nginx/nginx.conf \
+    --placeholder '@VALIDATION_DOMAIN'
+
 # Test nginx service
 nginx -t
 
