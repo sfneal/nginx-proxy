58c28a7e4f577026ace378cfd2bf8d84d532e47d
mrstephenneal <stephen.neal14@gmail.com>
2019-09-20 13:29:31 -0400

    ADD v9 image build and push scripts

--- push.sh
+++ push.sh
@@ -10,3 +10,4 @@ docker push stephenneal/nginx-proxy:1.17-alpine-v5
 docker push stephenneal/nginx-proxy:1.17-alpine-v6
 docker push stephenneal/nginx-proxy:1.17-alpine-v7
 docker push stephenneal/nginx-proxy:1.17-alpine-v8
+docker push stephenneal/nginx-proxy:1.17-alpine-v9
