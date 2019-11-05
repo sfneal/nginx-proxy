bc4a943cc19d10cad04e1d3f3d25d702098b8357
mrstephenneal <stephen.neal14@gmail.com>
2019-11-05 13:35:42 -0500

    ADD absolute directory resolving to build and push shell scripts

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-docker build -t stephenneal/nginx-proxy:1.17-alpine-v1 ./1.17-alpine-v1/
-docker build -t stephenneal/nginx-proxy:1.17-alpine-v2 ./1.17-alpine-v2/
-docker build -t stephenneal/nginx-proxy:1.17-alpine-v3 ./1.17-alpine-v3/
-docker build -t stephenneal/nginx-proxy:1.17-alpine-v4 ./1.17-alpine-v4/
-docker build -t stephenneal/nginx-proxy:1.17-alpine-v5 ./1.17-alpine-v5/
-docker build -t stephenneal/nginx-proxy:1.17-alpine-v6 ./1.17-alpine-v6/
-docker build -t stephenneal/nginx-proxy:1.17-alpine-v7 ./1.17-alpine-v7/
-docker build -t stephenneal/nginx-proxy:1.17-alpine-v8 ./1.17-alpine-v8/
-docker build -t stephenneal/nginx-proxy:1.17-alpine-v9 ./1.17-alpine-v9/
-docker build -t stephenneal/nginx-proxy:1.17-alpine-v10 ./1.17-alpine-v10/
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+docker build -t stephenneal/nginx-proxy:1.17-alpine-v1 "${DIR}"/1.17-alpine-v1/
+docker build -t stephenneal/nginx-proxy:1.17-alpine-v2 "${DIR}"/1.17-alpine-v2/
+docker build -t stephenneal/nginx-proxy:1.17-alpine-v3 "${DIR}"/1.17-alpine-v3/
+docker build -t stephenneal/nginx-proxy:1.17-alpine-v4 "${DIR}"/1.17-alpine-v4/
+docker build -t stephenneal/nginx-proxy:1.17-alpine-v5 "${DIR}"/1.17-alpine-v5/
+docker build -t stephenneal/nginx-proxy:1.17-alpine-v6 "${DIR}"/1.17-alpine-v6/
+docker build -t stephenneal/nginx-proxy:1.17-alpine-v7 "${DIR}"/1.17-alpine-v7/
+docker build -t stephenneal/nginx-proxy:1.17-alpine-v8 "${DIR}"/1.17-alpine-v8/
+docker build -t stephenneal/nginx-proxy:1.17-alpine-v9 "${DIR}"/1.17-alpine-v9/
+docker build -t stephenneal/nginx-proxy:1.17-alpine-v10 "${DIR}"/1.17-alpine-v10/
--- push.sh
+++ push.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-sh ./build.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+sh "${DIR}"/build.sh
 
 docker push stephenneal/nginx-proxy:1.17-alpine-v1
 docker push stephenneal/nginx-proxy:1.17-alpine-v2
