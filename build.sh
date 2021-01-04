12066a857f186a021efaa3f71aa022672b566efb
Stephen Neal <stephen.neal14@gmail.com>
2021-01-04 12:19:46 -0500

    OPTIMIZE Travis CI config by adding env matrix & param passing to build.sh script
    FIX build.sh to accept arguments.

--- build.sh
+++ build.sh
@@ -1,16 +1,30 @@
 #!/usr/bin/env bash
 
+# Base directory containing source code
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker build -t stephenneal/nginx-proxy:1.17-alpine-v1 "${DIR}"/1.17-alpine-v1/
-docker build -t stephenneal/nginx-proxy:1.17-alpine-v2 "${DIR}"/1.17-alpine-v2/
-docker build -t stephenneal/nginx-proxy:1.17-alpine-v5 "${DIR}"/1.17-alpine-v5/
-docker build -t stephenneal/nginx-proxy:1.17-alpine-v6 "${DIR}"/1.17-alpine-v6/
-docker build -t stephenneal/nginx-proxy:1.17-alpine-v7 "${DIR}"/1.17-alpine-v7/
-docker build -t stephenneal/nginx-proxy:1.17-alpine-v8 "${DIR}"/1.17-alpine-v8/
-docker build -t stephenneal/nginx-proxy:1.17-alpine-v9 "${DIR}"/1.17-alpine-v9/
-docker build -t stephenneal/nginx-proxy:1.17-alpine-v10 "${DIR}"/1.17-alpine-v10/
-docker build -t stephenneal/nginx-proxy:1.17-alpine-v11 "${DIR}"/1.17-alpine-v11/
-docker build -t stephenneal/nginx-proxy:1.17-alpine-v12 "${DIR}"/1.17-alpine-v12/
-docker build -t stephenneal/nginx-proxy:1.18-alpine-v1 "${DIR}"/1.18-alpine-v1/
-docker build -t stephenneal/nginx-proxy:1.19-alpine-v1 "${DIR}"/1.19-alpine-v1/
+# Optional TAG argument (if set, only the specified image will be built)
+TAG=${1:-null}
+
+# Check if the TAG variable is set
+if [ -z "$TAG" ]
+
+  # Only build one image
+  then
+    docker build -t stephenneal/nginx-proxy:"${TAG}" "${DIR}"/"${TAG}"/
+
+  # Build all images
+  else
+    docker build -t stephenneal/nginx-proxy:1.17-alpine-v1 "${DIR}"/1.17-alpine-v1/
+    docker build -t stephenneal/nginx-proxy:1.17-alpine-v2 "${DIR}"/1.17-alpine-v2/
+    docker build -t stephenneal/nginx-proxy:1.17-alpine-v5 "${DIR}"/1.17-alpine-v5/
+    docker build -t stephenneal/nginx-proxy:1.17-alpine-v6 "${DIR}"/1.17-alpine-v6/
+    docker build -t stephenneal/nginx-proxy:1.17-alpine-v7 "${DIR}"/1.17-alpine-v7/
+    docker build -t stephenneal/nginx-proxy:1.17-alpine-v8 "${DIR}"/1.17-alpine-v8/
+    docker build -t stephenneal/nginx-proxy:1.17-alpine-v9 "${DIR}"/1.17-alpine-v9/
+    docker build -t stephenneal/nginx-proxy:1.17-alpine-v10 "${DIR}"/1.17-alpine-v10/
+    docker build -t stephenneal/nginx-proxy:1.17-alpine-v11 "${DIR}"/1.17-alpine-v11/
+    docker build -t stephenneal/nginx-proxy:1.17-alpine-v12 "${DIR}"/1.17-alpine-v12/
+    docker build -t stephenneal/nginx-proxy:1.18-alpine-v1 "${DIR}"/1.18-alpine-v1/
+    docker build -t stephenneal/nginx-proxy:1.19-alpine-v1 "${DIR}"/1.19-alpine-v1/
+fi
