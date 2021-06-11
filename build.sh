b36f38f4b006c2629fbc70c9f23cd591dae1d337
Stephen Neal <stephen.neal14@gmail.com>
2021-06-11 14:27:46 -0400

    ADD support for nginx versions v1.20 & v1.21

--- build.sh
+++ build.sh
@@ -27,4 +27,6 @@ if [ "$TAG" != null ]
     docker build -t stephenneal/nginx-proxy:1.17-alpine-v12 "${DIR}"/1.17-alpine-v12/
     docker build -t stephenneal/nginx-proxy:1.18-alpine-v1 "${DIR}"/1.18-alpine-v1/
     docker build -t stephenneal/nginx-proxy:1.19-alpine-v1 "${DIR}"/1.19-alpine-v1/
+    docker build -t stephenneal/nginx-proxy:1.20-alpine-v1 "${DIR}"/1.20-alpine-v1/
+    docker build -t stephenneal/nginx-proxy:1.21-alpine-v1 "${DIR}"/1.21-alpine-v1/
 fi
--- push.sh
+++ push.sh
@@ -29,4 +29,6 @@ if [ "$TAG" != null ]
     docker push stephenneal/nginx-proxy:1.17-alpine-v12
     docker push stephenneal/nginx-proxy:1.18-alpine-v1
     docker push stephenneal/nginx-proxy:1.19-alpine-v1
+    docker push stephenneal/nginx-proxy:1.20-alpine-v1
+    docker push stephenneal/nginx-proxy:1.21-alpine-v1
 fi
